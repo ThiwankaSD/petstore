e0abe0c30b12e850985b8c416e34111391a8e9bc
Thiwanka Sahan <thiwankasahandissanayaka@gmail.com>
2021-11-19 12:37:50 +0530

    Update Rest Application definition

--- src/main/java/com/example/petstore/PetstoreRestApplication.java
+++ src/main/java/com/example/petstore/PetstoreRestApplication.java
@@ -4,8 +4,10 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
- *
+ * This is the starting point of the application
+ * Application Path depicts the entry point: default is /
  */
 @ApplicationPath("/")
 public class PetstoreRestApplication extends Application {
+
 }
