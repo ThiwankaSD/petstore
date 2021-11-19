b66af7b56f26dc5fef9272d8da27b7a1c39828e2
Thiwanka Sahan <thiwankasahandissanayaka@gmail.com>
2021-11-19 13:12:45 +0530

    Define Model classes

--- src/main/java/com/example/petstore/Pet.java
+++ src/main/java/com/example/petstore/Pet.java
@@ -1,5 +1,7 @@
 package com.example.petstore;
 
+import java.util.Objects;
+
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -15,11 +17,11 @@ public class Pet {
 	@JsonProperty("pet_type")
 	private String petType;
 
-	@Schema(required = true, description = "Pet name")
-	@JsonProperty("pet_name")
+	@Schema(required = true, description = "Pet petName")
+	@JsonProperty("pet_petName")
 	private String petName;
 
-	@JsonProperty("pet_age")
+	@JsonProperty("pet_petAge")
 	private Integer petAge;
 
 	public Integer getPetId() {
@@ -53,5 +55,47 @@ public class Pet {
 	public void setPetAge(Integer petAge) {
 		this.petAge = petAge;
 	}
+	@Override
+	  public boolean equals(java.lang.Object o) {
+	    if (this == o) {
+	      return true;
+	    }
+	    if (o == null || getClass() != o.getClass()) {
+	      return false;
+	    }
+	    Pet pet = (Pet) o;
+	    return Objects.equals(petId, pet.petId) &&
+	        Objects.equals(petType, pet.petType) &&
+	        Objects.equals(petName, pet.petName) &&
+	        Objects.equals(petAge, pet.petAge);
+	  }
 
+	  @Override
+	  public int hashCode() {
+	    return Objects.hash(petId, petType, petName, petAge);
+	  }
+
+	  @Override
+	  public String toString() {
+	    StringBuilder sb = new StringBuilder();
+	    sb.append("class Pet {\n");
+	    
+	    sb.append("    id: ").append(toIndentedString(petId)).append("\n");
+	    sb.append("    petType: ").append(toIndentedString(petType)).append("\n");
+	    sb.append("    petName: ").append(toIndentedString(petName)).append("\n");
+	    sb.append("    petAge: ").append(toIndentedString(petAge)).append("\n");
+	    sb.append("}");
+	    return sb.toString();
+	  }
+
+	  /**
+	   * Convert the given object to string with each line indented by 4 spaces
+	   * (except the first line).
+	   */
+	  private String toIndentedString(java.lang.Object o) {
+	    if (o == null) {
+	      return "null";
+	    }
+	    return o.toString().replace("\n", "\n    ");
+	  }
 }
---
+++ src/main/java/com/example/petstore/PetType.java
@@ -0,0 +1,92 @@
+package com.example.petstore;
+
+import java.util.Objects;
+
+import javax.validation.Valid;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Schema(name = "PetType")
+public class PetType   {
+
+  @Schema(required = true, description = "PetType id")	
+  private @Valid Long id = null;
+
+  @Schema(required = true, description = "PetType name")
+  private @Valid String name = null;
+
+  /**
+   **/
+  public PetType id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  @JsonProperty("id")
+
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  /**
+   **/
+  public PetType name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  @JsonProperty("name")
+
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PetType petType = (PetType) o;
+    return Objects.equals(id, petType.id) &&
+        Objects.equals(name, petType.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class PetType {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
